4176e15a5290057bd3547fce791266dd4a0f4703
Matin <matin.parsapour.iam@gmail.com>
2022-08-11 19:57:09 +0430

    Create base project for revised simplex algorithm

---
+++ src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/RevisedSimplexApplication.java
@@ -0,0 +1,13 @@
+package ir.algorithm.revisedSimplex.RevisedSimplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisedSimplexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisedSimplexApplication.class, args);
+	}
+
+}

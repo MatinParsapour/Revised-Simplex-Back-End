ca51297c54ad6eb79401494638e2f89f6134c90d
Matin <matin.parsapour.iam@gmail.com>
2022-08-12 14:26:01 +0430

    Create a super class for all the data that should be imported to service

---
+++ src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/model/Data.java
@@ -0,0 +1,11 @@
+package ir.algorithm.revisedSimplex.RevisedSimplex.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class Data<T> {
+
+    private T data;
+}

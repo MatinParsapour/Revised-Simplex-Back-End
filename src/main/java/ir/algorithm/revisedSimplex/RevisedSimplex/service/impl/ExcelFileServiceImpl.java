21b7e78dd07d4eeedd93a2fbaf145a095ac82463
Matin <matin.parsapour.iam@gmail.com>
2022-08-12 14:28:04 +0430

    Implement extract method in DataService

---
+++ src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/service/impl/ExcelFileServiceImpl.java
@@ -0,0 +1,14 @@
+package ir.algorithm.revisedSimplex.RevisedSimplex.service.impl;
+
+import ir.algorithm.revisedSimplex.RevisedSimplex.model.File;
+import ir.algorithm.revisedSimplex.RevisedSimplex.service.ExcelFileService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExcelFileServiceImpl implements ExcelFileService {
+
+    @Override
+    public void extract(File data) {
+
+    }
+}

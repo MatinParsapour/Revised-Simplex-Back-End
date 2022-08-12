636bc6b7f0be89c6fc05c25906dd91c7c0f76134
Matin <matin.parsapour.iam@gmail.com>
2022-08-12 14:44:46 +0430

    Create controller for Value class

---
+++ src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/controller/ValueController.java
@@ -0,0 +1,9 @@
+package ir.algorithm.revisedSimplex.RevisedSimplex.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/value")
+public class ValueController {
+}

4d90bcaab28500af6393974aa03ed54c9dc8f07b
Matin <matin.parsapour.iam@gmail.com>
2022-08-12 14:26:42 +0430

    Create a File class that extends Data class

---
+++ src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/model/File.java
@@ -0,0 +1,9 @@
+package ir.algorithm.revisedSimplex.RevisedSimplex.model;
+
+import lombok.Builder;
+import org.springframework.web.multipart.MultipartFile;
+
+@Builder
+public abstract class File extends Data<MultipartFile>{
+
+}

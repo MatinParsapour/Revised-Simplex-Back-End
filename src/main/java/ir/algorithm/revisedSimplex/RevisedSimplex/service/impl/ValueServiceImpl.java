4339653293faac9647fea631ab7ce6e505a581c5
Matin <matin.parsapour.iam@gmail.com>
2022-08-12 14:45:38 +0430

    Implement extract method for Value children

---
+++ src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/service/impl/ValueServiceImpl.java
@@ -0,0 +1,14 @@
+package ir.algorithm.revisedSimplex.RevisedSimplex.service.impl;
+
+import ir.algorithm.revisedSimplex.RevisedSimplex.model.Value;
+import ir.algorithm.revisedSimplex.RevisedSimplex.service.ValueService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ValueServiceImpl implements ValueService {
+
+    @Override
+    public void extract(Value data) {
+
+    }
+}

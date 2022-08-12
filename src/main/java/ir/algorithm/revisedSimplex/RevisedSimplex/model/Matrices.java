e835a53c76c19c67ab8867de87cc7f707794e490
Matin <matin.parsapour.iam@gmail.com>
2022-08-12 14:43:22 +0430

    Create a class named Matrices to save all matrices user enteres

---
+++ src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/model/Matrices.java
@@ -0,0 +1,23 @@
+package ir.algorithm.revisedSimplex.RevisedSimplex.model;
+
+import lombok.*;
+
+import java.util.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Matrices {
+
+    private final List<Double> coefficientsOfTargetFunction = new LinkedList<>();
+    private List<Double> basicVariablesInTargetFunction;
+    private List<Double> nonBasicVariablesInTargetFunction;
+    private final List<List<Double>> coefficientsOfConstraints = new LinkedList<>();
+    private final List<Double> coefficientsOfResults = new LinkedList<>();
+    private Map<Integer, List<Double>> basicVariables = new TreeMap<>();
+    private Map<Integer, List<Double>> nonBasicVariables = new TreeMap<>();
+
+
+}

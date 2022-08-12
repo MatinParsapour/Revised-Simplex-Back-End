3bf11cb682f930d308953317f84598037020f17c
Matin <matin.parsapour.iam@gmail.com>
2022-08-12 14:28:43 +0430

    Update imports and annotation of file controller

--- src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/controller/FileController.java
+++ src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/controller/FileController.java
@@ -1,14 +1,30 @@
 package ir.algorithm.revisedSimplex.RevisedSimplex.controller;
 
-import org.springframework.stereotype.Controller;
+import ir.algorithm.revisedSimplex.RevisedSimplex.service.ExcelFileService;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+import java.io.IOException;
+import java.util.Iterator;
+
+@RestController
 @RequestMapping("/file")
 public class FileController {
+
+    private final ExcelFileService excelFileService;
+
+    @Autowired
+    public FileController(ExcelFileService excelFileService) {
+        this.excelFileService = excelFileService;
+    }
 
     @PostMapping("/import-excel")
     public void importExcelFile(@RequestParam("file")MultipartFile file) {

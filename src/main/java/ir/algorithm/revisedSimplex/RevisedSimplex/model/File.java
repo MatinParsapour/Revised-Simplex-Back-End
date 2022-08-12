bd455dd289d824702be412c191790452517c8462
Matin <matin.parsapour.iam@gmail.com>
2022-08-12 15:41:10 +0430

    Remove builder annotation from File class

--- src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/model/File.java
+++ src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/model/File.java
@@ -1,9 +1,7 @@
 package ir.algorithm.revisedSimplex.RevisedSimplex.model;
 
-import lombok.Builder;
 import org.springframework.web.multipart.MultipartFile;
 
-@Builder
 public abstract class File extends Data<MultipartFile>{
 
 }

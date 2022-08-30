f540f363513d6711ed105fa1a06a189457be0e81
Matin <matin.parsapour.iam@gmail.com>
2022-08-30 20:48:28 +0430

    Create an interface to calculate and return optimal value

---
+++ src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/service/OptimalService.java
@@ -0,0 +1,10 @@
+package ir.algorithm.revisedSimplex.RevisedSimplex.service;
+
+import java.util.List;
+
+public interface OptimalService {
+
+    List<Double> getOptimalVariable();
+
+    double getOptimalResult();
+}

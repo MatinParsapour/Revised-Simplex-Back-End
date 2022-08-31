31d7f01806bd84abd7a808281f8416af0b2fbc36
Matin <matin.parsapour.iam@gmail.com>
2022-08-31 20:39:18 +0430

    Create an interface to extract data inside the object user send to server

--- src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/service/ValueService.java
+++ src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/service/ValueService.java
@@ -1,8 +1,9 @@
 package ir.algorithm.revisedSimplex.RevisedSimplex.service;
 
+import ir.algorithm.revisedSimplex.RevisedSimplex.model.MatricesValues;
 import ir.algorithm.revisedSimplex.RevisedSimplex.model.Value;
 
-public interface ValueService<T extends Value> extends DataService<Value> {
+public interface ValueService<T extends MatricesValues> extends DataService<Value> {
 
     void extract(T data);
 }

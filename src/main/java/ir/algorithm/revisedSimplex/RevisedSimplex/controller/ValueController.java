b896ef6c8a85e2373d647bee82eedba4c9056bf7
Matin <matin.parsapour.iam@gmail.com>
2022-08-31 20:38:47 +0430

    Create a controller to get values from user

--- src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/controller/ValueController.java
+++ src/main/java/ir/algorithm/revisedSimplex/RevisedSimplex/controller/ValueController.java
@@ -1,9 +1,29 @@
 package ir.algorithm.revisedSimplex.RevisedSimplex.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ir.algorithm.revisedSimplex.RevisedSimplex.model.Data;
+import ir.algorithm.revisedSimplex.RevisedSimplex.model.Matrices;
+import ir.algorithm.revisedSimplex.RevisedSimplex.model.MatricesValues;
+import ir.algorithm.revisedSimplex.RevisedSimplex.model.Value;
+import ir.algorithm.revisedSimplex.RevisedSimplex.service.ValueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/value")
 public class ValueController {
+
+    private final ValueService<MatricesValues> valueService;
+
+    @Autowired
+    public ValueController(ValueService<MatricesValues> valueService) {
+        this.valueService = valueService;
+    }
+
+    @PostMapping("/send-data")
+    public void sendDate(@RequestBody MatricesValues values){
+        valueService.extract(values);
+    }
+
 }
